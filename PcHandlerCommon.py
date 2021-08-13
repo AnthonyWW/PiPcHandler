a2492beb21a37c29efb716134bb40574db683dae
WhiteHawk <anthony.white@polymtl.ca>
2021-08-13 14:34:01 -0400

    no message

--- PcHandlerCommon.py
+++ PcHandlerCommon.py
@@ -1,5 +1,16 @@
 import platform    # For getting the operating system name
 import subprocess  # For executing a shell command
+import os, sys
+
+class HiddenPrints:
+    def __enter__(self):
+        self._original_stdout = sys.stdout
+        sys.stdout = open(os.devnull, 'w')
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        sys.stdout.close()
+        sys.stdout = self._original_stdout
+
 
 def ping(host):
     """
@@ -7,12 +18,16 @@ def ping(host):
     Remember that a host may not respond to a ping (ICMP) request even if the host name is valid.
     """
 
-    # Option for the number of packets as a function of
-    param = '-n' if platform.system().lower()=='windows' else '-c'
+    with HiddenPrints:
+        # Option for the number of packets as a function of
+        param = '-n' if platform.system().lower()=='windows' else '-c'
 
-    # Building the command. Ex: "ping -c 1 google.com"
-    command = ['ping', param, '1', host, " 2>&1 >/dev/null"]
+        # Building the command. Ex: "ping -c 1 google.com"
+        command = ['ping', param, '1', host]
 
-    return subprocess.call(command) == 0
+        return subprocess.call(command) == 0
 
-    
+
+
+
+
